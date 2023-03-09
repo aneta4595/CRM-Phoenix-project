be16767896a0684cc9e9cf44130205d3e6ac3eb7
Aneta Walicka <aneta4595@wp.pl>
2023-03-09 20:19:20 +0100

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LeadsComponent } from './components/leads/leads.component';
+import { LoginComponentModule } from './components/login/login.component-module';
+import { LeadsComponentModule } from './components/leads/leads.component-module';
+import { IsLoggedInGuard } from './guards/is-logged-in/is-logged-in.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'leads', component: LeadsComponent, canActivate: [IsLoggedInGuard] },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    LoginComponentModule,
+    LeadsComponentModule,
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
