58de2d48e3daae1283679ff94c189dbe3f3700bf
Aneta Walicka <aneta4595@wp.pl>
2023-03-11 15:41:28 +0100

    routes added to app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { LeadsComponent } from './components/leads/leads.component';
+import { RegisterComponent } from './components/register/register.component';
+import { VerifyComponent } from './components/verify/verify.component';
 import { LoginComponentModule } from './components/login/login.component-module';
 import { LeadsComponentModule } from './components/leads/leads.component-module';
+import { RegisterComponentModule } from './components/register/register.component-module';
+import { VerifyComponentModule } from './components/verify/verify.component-module';
+import { VerifiedGuard } from './guards/verified/verified.guard';
 import { IsLoggedInGuard } from './guards/is-logged-in/is-logged-in.guard';
 
 const routes: Routes = [
   { path: 'auth/login', component: LoginComponent },
-  { path: 'leads', component: LeadsComponent, canActivate: [IsLoggedInGuard] },
+  { path: 'leads', component: LeadsComponent, canActivate: [IsLoggedInGuard, VerifiedGuard] },
+  { path: 'auth/register', component: RegisterComponent },
+  { path: 'verify', component: VerifyComponent }
 ];
 
 @NgModule({
@@ -16,7 +23,9 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     LoginComponentModule,
     LeadsComponentModule,
+    RegisterComponentModule,
+    VerifyComponentModule
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
