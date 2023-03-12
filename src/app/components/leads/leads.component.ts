7984ca999dfd708c5370bb87bac04da62e1ec0e0
Aneta Walicka <aneta4595@wp.pl>
2023-03-12 16:51:00 +0100

    leads-table added to leads component

--- src/app/components/leads/leads.component.ts
+++ src/app/components/leads/leads.component.ts
@@ -1,7 +1,12 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { BehaviorSubject, Observable, take, tap } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { LeadModel } from '../../models/lead.model';
+import { ActivityModel } from '../../models/activity.model';
 import { AuthService } from '../../services/auth.service';
+import { LeadsService } from '../../services/leads.service';
+import { LeadQueryModel } from 'src/app/query-models/lead.query-model';
 
 @Component({
   selector: 'app-leads',
@@ -13,7 +18,14 @@ export class LeadsComponent {
   private _hamburgerMenuUserSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   public hamburgerMenuUser$: Observable<boolean> = this._hamburgerMenuUserSubject.asObservable();
 
-  constructor(private _authService: AuthService, private _router: Router) {
+readonly leadsAndActivities$: Observable<LeadQueryModel[]> = combineLatest([
+  this._leadsService.getAllLeads(),
+  this._leadsService.getAllActivities()
+]).pipe(
+map(([leads, activities]: [LeadModel[], ActivityModel[]]) => this._mapToLeadQuery(leads, activities))
+)
+
+  constructor(private _authService: AuthService, private _router: Router, private _leadsService: LeadsService) {
   }
 
   logout(): void {
@@ -23,7 +35,28 @@ export class LeadsComponent {
 
   hamburgerMenuUser() {
     this.hamburgerMenuUser$.pipe(take(1),
-    tap((isShow) => this._hamburgerMenuUserSubject.next(!isShow))).subscribe()
+      tap((isShow) => this._hamburgerMenuUserSubject.next(!isShow))).subscribe()
   }
 
+
+  private _mapToLeadQuery(leads: LeadModel[], activities: ActivityModel[]): LeadQueryModel[] {
+    const activitiesMap = activities.reduce((a,c) => ({...a, [c.id]: c}),
+    
+    {}
+    ) as Record <string, ActivityModel>;
+
+    return leads.map((l) => {
+      return {
+        location: l.location,
+        companySize: l.companySize,
+        hiring: l.hiring,
+        websiteLink: l.websiteLink,
+        linkedinLink: l.linkedinLink,
+        industry: l.industry,
+        name: l.name,
+        annualRevenue: l.annualRevenue,
+        scopes: (l.activityIds ?? []).map((id) => activitiesMap[id]?.name)
+      }
+    })
+  }
 }
